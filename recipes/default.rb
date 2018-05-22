92370b37b995107cdb703458c9e2f3613b15ecbe
FirstName LastName <you@your_provider.com>
2018-05-22 16:20:42 +0000

    Added commentary for using the reboot resource. Added InSpec tests

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,6 +6,7 @@
 
 reboot 'Restarting for fun and profit' do
   action :nothing
+    # Note: Do _NOT_ set it to :reboot_now w/o adding guards for it, or it will reboot every time you run this recipe. (_entire_ run_list is re-ran on reboot, will loop indefinitely otherwise)
   reason 'Because I can'
   delay_mins 1
 end
