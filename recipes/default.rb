e47fed2d2b151f6465708b2614107e3c83cf592c
FirstName LastName <you@your_provider.com>
2018-05-22 15:42:52 +0000

    Test resource that creates a file, then reboots.

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,17 @@
 # Recipe:: default
 #
 # Copyright:: 2018, Robert Miesen, All Rights Reserved.
+
+reboot 'Restarting for fun and profit' do
+  action :nothing
+  reason 'Because I can'
+  delay_mins 1
+end
+
+file 'C:\hello.txt' do
+  content 'Chef is going to reboot your server. Ha ha!'
+
+  action :create
+
+  notifies :reboot_now, 'reboot[Restarting for fun and profit]', :immediately
+end
